1c7d6665c8efdb1c1bc6a95b0027daa5553893e6
lm83128 <lm83128@imcap.ap.ssmb.com>
2018-02-01 16:57:40 +0800

    add raw loader

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ module.exports = {
                 test: /\.(png|jpg|gif|woff|woff2|eot|ttf|svg)$/, 
                 loader: 'url-loader?limit=100000' 
             },
+            { test: /\.html$/, loader: 'raw-loader' },
         ]
     }
 };
